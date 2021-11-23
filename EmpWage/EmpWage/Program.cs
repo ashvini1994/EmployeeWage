eca4ea6d26dc83490ad6c34db390e4756393f5b8
Ashu <ashusarnaik11@gmail.com>
2021-11-24 00:03:43 +0530

    Attendance

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -11,7 +11,20 @@ namespace EmpWage
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to All");
-            Console.ReadKey();
+            //Constant
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+                Console.ReadKey();
         }
     }
 }
