7f244e9ba4840c30b32d442beca2045a07bf57c6
Ashu <ashusarnaik11@gmail.com>
2021-11-24 00:50:47 +0530

    part time

--- EmpWage/EmpWage/Program.cs
+++ EmpWage/EmpWage/Program.cs
@@ -11,15 +11,20 @@ namespace EmpWage
         static void Main(string[] args)
         {
             //Constant
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             //Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
-            int empCheck = random.Next(0, 2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
             }
@@ -30,6 +35,7 @@ namespace EmpWage
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
             Console.ReadKey();
+
         }
     }
 }
